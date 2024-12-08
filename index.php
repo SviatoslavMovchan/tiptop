e4efe2a5bc9d4048b3c1b61c88783cb42a89adad
SviatoslavMovchan <movchan_sviatoslav@vpu21.if.ua>
2024-12-08 23:02:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Візитка магазину ТІПТОП</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+   <header>
+        <img src="images/photo_2024-12-08_21-46-03.jpg" alt="Банер магазину" class="banner">
+    </header>
+
+  
+    <section class="about">
+        <h1>Ласкаво просимо до магазину ТІПТОП!</h1>
+        <p>Наш магазин знаходиться за адресою: <strong>м. Київ, вул. Прикладна, 12</strong></p>
+        <p>Години роботи: з 09:00 до 18:00 (понеділок - п'ятниця)</p>
+    </section>
+
+    
+    <section class="products">
+        <h2>Наші товари</h2>
+        <div class="product-list">
+
+            <div class="product-card">
+                <img src="images/product1.jpg" alt="Товар 1">
+                <h3>Товар 1</h3>
+                <p>Ціна: 8 941 грн</p>
+            </div>
+
+            <div class="product-card">
+                <img src="images/product2.jpg" alt="Товар 2">
+                <h3>Товар 2</h3>
+                <p>Ціна: 16 556 грн</p>
+            </div>
+
+            <div class="product-card">
+                <img src="images/product3.jpg" alt="Товар 3">
+                <h3>Товар 3</h3>
+                <p>Ціна: 31 999 грн</p>
+            </div>
+
+        </div>
+    </section>
+    <footer>
+        <p>© 2024 ТІПТОП. Всі права захищено.</p>
+    </footer>
+
+</body>
+</html>
