cb5d8b9d2322954b137d7cdd004819fcf3f54ac6
SviatoslavMovchan <movchan_sviatoslav@vpu21.if.ua>
2024-12-08 23:55:54 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -15,8 +15,12 @@
   
     <section class="about">
         <h1>Ласкаво просимо до магазину ТІПТОП!</h1>
-        <p>Наш магазин знаходиться за адресою: <strong>м. Київ, вул. Прикладна, 12</strong></p>
+        <p>Наш магазин знаходиться за адресою: <strong>м. Івано-Франківськ, вул. Бельведерська, 25А</strong></p>
         <p>Години роботи: з 09:00 до 18:00 (понеділок - п'ятниця)</p>
+        <p>Години роботи: з 10:00 до 15:00 (субота)</p>
+        <p>Вихідний:неділя</p>
+        <p>Номер телефону:<a href="tel:+380977353830">(097) 735-38-30</a><br></p>
+        <p>Номер телефону:(095)488-37-05;  (097)735-38-30</p>
     </section>
 
     
@@ -26,19 +30,22 @@
 
             <div class="product-card">
                 <img src="images/product1.jpg" alt="Товар 1">
-                <h3>Товар 1</h3>
+                <h3><a href="https://tiptop.ua/shop/tovar-2090182-planshet-xiaomi-redmi-pad-se-8-256gb-graphite-gray-vhu4587eu-1022988" target="_blank">
+                    Планшет Xiaomi Redmi Pad SE 8/256GB Graphite Gray</h3>
                 <p>Ціна: 8 941 грн</p>
             </div>
 
             <div class="product-card">
                 <img src="images/product2.jpg" alt="Товар 2">
-                <h3>Товар 2</h3>
-                <p>Ціна: 16 556 грн</p>
+                <h3><a href="https://tiptop.ua/shop/tovar-2053626-smartfon-samsung-galaxy-a55-5g-8-128gb-zka-awesome-navy" target="_blank">
+                    Мобільний телефон Samsung Galaxy A55 5G 8/128Gb Awesome Navy</h3>
+                <p>Ціна: 14 238 грн</p>
             </div>
 
             <div class="product-card">
                 <img src="images/product3.jpg" alt="Товар 3">
-                <h3>Товар 3</h3>
+                <h3><a href="https://tiptop.ua/shop/tovar-2304683-noutbuk-asus-tuf-gaming-a15-fa506nfr-hn081-90nr0ju7-m00530" target="_blank">
+                    Ноутбук ASUS TUF Gaming A15 FA506NFR-HN081</h3>
                 <p>Ціна: 31 999 грн</p>
             </div>
 
